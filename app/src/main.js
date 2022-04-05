2faf2a90a6da5029bea85e39fabe10502ead8256
Fredrik Sjölander <f.sjolander@gmail.com>
2022-04-05 14:16:28 +0200

    Added ResponseContainer class and the QueryManager file

--- app/src/main.js
+++ app/src/main.js
@@ -3,6 +3,20 @@ const awilix = require('awilix')
 const container = awilix.createContainer()
 
 container.register({
+    /********************************************** TESTS **********************************************/
+    // Add test files here...
+
+    /******************************************* DATA ACCESS *******************************************/
+    dbConnection:   awilix.asFunction(require('./data-access-layer/db')),
+    queryManager:   awilix.asFunction(require('./data-access-layer/query-manager')),
+
+    /***************************************** BUSINESS LOGIC ******************************************/
+    // Add BLL files here...
+
+    /*********************************************** API ***********************************************/
+    // Add API related files here...
+
+    /******************************************* APPLICATION *******************************************/
     app:            awilix.asFunction(require('./api/index'))
 })
 
