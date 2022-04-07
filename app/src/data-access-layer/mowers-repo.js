b274e68aa30720b1d853d6de27bab2a6ef32fcc1
Fredrik Sjölander <f.sjolander@gmail.com>
2022-04-07 16:40:59 +0200

    Started implementing Mower class

---
+++ app/src/data-access-layer/mowers-repo.js
@@ -0,0 +1,24 @@
+module.exports = function({QueryManager}) {
+
+    const exports = {}
+    const resourceName = 'mowers'
+
+    /**
+     * Gets all mowers with the specified user id.
+     * @param {number} user_id
+     * @returns {Promise<ResponseContainer>}
+     */
+    exports.getMowersByUserId = async function(user_id) {
+
+
+    }
+
+
+    exports.getMowerBySerial = async function(mower_serial) {
+
+
+    }
+
+
+    return exports
+}
