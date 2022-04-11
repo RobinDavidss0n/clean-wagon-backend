f2ba00aedadf3fc77d81fe553e9028f8590935b7
Fredrik Sjölander <f.sjolander@gmail.com>
2022-04-11 16:11:03 +0200

    Added id and event, plus getters and setters to the coordinate class

--- app/src/data-access-layer/classes/coordinate-class.js
+++ app/src/data-access-layer/classes/coordinate-class.js
@@ -1,14 +1,42 @@
 // const constants = require('../../business-logic-layer/constants')
+const Event = require('./event-class')
 
 module.exports = class Coordinate {
+    id
     x
     y
     journey_id
+    event = null
 
     constructor(x, y, journey_id) {
         this.x = x
         this.y = y
         this.journeyId = journey_id
+    }
+
+
+    /**
+     * Returns the id
+     * @returns {number}
+     */
+    get id() {
+        return this.id
+    }
+
+    /**
+     * Sets the event
+     * @param {Event} event
+     */
+    set event(event) {
+        this.event = event
+    }
+
+    /**
+     * Returns the event
+     * @returns {Event}
+     */
+    get event() {
+        return this.event
     }
 
     /**
